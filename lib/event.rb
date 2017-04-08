1b6db0834be5b83ccf4d3886dcde97469cbbb207
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-04-08 14:51:41 +0100

    Create other attributes of event

--- lib/event.rb
+++ lib/event.rb
@@ -1,16 +1,28 @@
 require 'json'
 
 class Event
-  attr_reader :id
+  attr_reader :id, :no_tickets, :price, :coords
 
   def initialize(id)
     @id = id
+    @no_tickets = nil
+    @price = nil
+    @coords = nil
+    load_events
   end
 
+  private
 
   def load_events
     json = File.read('event_data.json')
     events = JSON.parse(json)
+    events.each do |event|
+      if event["id"] == @id
+        @no_tickets = event["no_tickets"]
+        @price = event["price"]
+        @coords = event["x_coord"], event["y_coord"]
+      end
+    end
   end
 
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,9 +3,22 @@ require 'event'
 describe Event do
   subject(:event) { Event.new(1) }
 
-  it 'Creates new event with id' do
-    p event
-    expect(event.id).to eq(1)
+  describe 'attributes' do
+    it 'Has a unique id' do
+      expect(event.id).to eq(1)
+    end
+
+    it 'Has a number of tickets' do
+      expect(event.no_tickets).to eq(57)
+    end
+
+    it 'Has a price' do
+      expect(event.price).to eq("$54.38")
+    end
+
+    it 'Has a pair of coordinates' do
+      expect(event.coords).to eq([4, 0])
+    end
   end
 
 end
