5b65125634ed5e55f13a5300a1f94c83ecab67c7
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-04-08 16:06:05 +0100

    Create grid array of events

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,10 +1,14 @@
 class Grid
-  attr_accessor :cells
+  attr_accessor :cells, :cells_with_events
 
   def initialize
     @cells = {}
+    @cells_with_events = []
     create_cells
+    find_cells_with_events
   end
+
+  private
 
   def create_cells
     (-10..10).each do |x|
@@ -12,6 +16,12 @@ class Grid
         coords="#{x}, #{y}"
         @cells[coords]=(Cell.new(x,y))
       end
+    end
+  end
+
+  def find_cells_with_events
+    @cells.each do |c|
+      @cells_with_events.push(c) if c[1].event != nil
     end
   end
 
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -7,6 +7,10 @@ describe Grid do
     it 'Creates a new grid, -10 to +10 (Y axis) and -10 to +10 (X axis)' do
       expect(grid.cells.count).to eq(441)
     end
+
+    it 'Stores a list of cells with events in' do
+      expect(grid.cells_with_events).not_to be_empty
+    end
   end
 
 end
