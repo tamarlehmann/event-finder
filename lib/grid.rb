fba5849214d2ae568edbfbcef2912a9826ee003e
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-04-08 13:16:20 +0100

    Grid creates cells with coords on initialization

--- lib/grid.rb
+++ lib/grid.rb
@@ -2,12 +2,16 @@ class Grid
   attr_accessor :cells
 
   def initialize
-    @cells = []
+    @cells = {}
+    create_cells
   end
 
   def create_cells
-    441.times do
-      @cells.push("cell")
+    (-10..10).each do |x|
+      (-10..10).each do |y|
+        coords="#{x},#{y}"
+        @cells[coords]=(Cell.new(x,y))
+      end
     end
   end
 
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -5,9 +5,8 @@ describe Grid do
 
   describe '::new' do
     it 'Creates a new grid, -10 to +10 (Y axis) and -10 to +10 (X axis)' do
-      grid.create_cells
       expect(grid.cells.count).to eq(441)
     end
   end
-  
+
 end
